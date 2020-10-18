36c17b926f1dd3bd5f105b4119bc1464c365ae1b
Tim Triche, Jr <tim.triche@gmail.com>
2020-10-18 18:57:42 -0400

    more velocity updates

--- R/compute_velocity.R
+++ R/compute_velocity.R
@@ -16,7 +16,7 @@ compute_velocity <- function(txis, ...) {
   message("Removing dead cells and low-variance genes for velocity")
   mt <- names(subset(rowRanges(txis), seqnames %in% c("chrM", "chrMT", "MT")))
   txis$mtPercent <- (colSums(counts(txis[mt,])) / colSums(counts(txis))) * 100
-  mtCut <- max(quantile(txis$mtPercent, 0.95), 10)
+  mtCut <- max(quantile(txis$mtPercent, 0.95), 10, na.rm=TRUE)
   live <- colnames(txis)[txis$mtPercent < mtCut]
   dec <- modelGeneVar(txis[, live])
   HVGs <- scran::getTopHVGs(dec, n=1000)
@@ -36,7 +36,7 @@ compute_velocity <- function(txis, ...) {
       txis <- runUMAP(txis, ncomponents=3)
     } 
   }
-  embedded <- embedVelocity(reducedDim(txis, "UMAP"), metadata(txis)$scVelo)
+  embedded <- embedVelocity(reducedDims(txis)$UMAP, metadata(txis)$scVelo)
   metadata(txis)$embedded <- embedded
 
   message("Added scVelo (stochastic mode) output to metadata(txis)$scVelo")
