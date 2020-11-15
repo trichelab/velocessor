1446a7a06e72cf5cc535ee767821da63002d0a45
Tim Triche, Jr <tim.triche@gmail.com>
2020-11-15 16:07:49 -0500

    bugfix in label_cells

--- R/label_cells.R
+++ R/label_cells.R
@@ -4,7 +4,7 @@
 #' directly with SingleR input and output for (e.g.) specific gene sets.
 #' 
 #' @param txis        a SingleCellExperiment
-#' @param species     what species is this from? ("Homo sapiens" -- automate?) 
+#' @param species     what species is this from? (autodetect Hs/Mm) 
 #' @param ret         what kind of object to return
 #' @param downsample  downsample? (if ncol(txis) > 20000, TRUE, else FALSE) 
 #' @param maxcells    maximum number of cells per cluster per sample (50)
@@ -12,22 +12,28 @@
 #'
 #' @return depending on the value of 'ret', either an SCE or a set of labels
 #' 
+#' @details
+#' Autodetection of species will fail if the genome for the SingleCellExperiment
+#' does not contain "GRCm", "mm", "GRCh", or "hg". 
+#' 
 #' @import SingleR 
 #'
 #' @export
-label_cells <- function(txis, species=c("Homo sapiens", "Mus musculus"), ret=c("sce", "labels"), downsample=NULL, maxcells=50, ...) {
+label_cells <- function(txis, species=NULL, ret=c("sce", "labels"), downsample=NULL, maxcells=50, ...) {
 
   stopifnot(is(txis, "SingleCellExperiment"))
+  if (is.null(species)) {
+    if (grepl("^(GRCh|hg)", unique(genome(txis)))) {
+      species <- "Homo sapiens"
+    } else if (grepl("^(GRCm|mm)", unique(genome(txis)))) {
+      species <- "Mus musculus"
+    } else if (grepl("^(GRCz|dr)", unique(genome(txis)))) {
+      species <- "Danio rerio"
+    } else { 
+      stop("Cannot automatically determine species to label cells. Exiting.") 
+    } 
+  } 
 
-  ret <- match.arg(ret)
-  species <- match.arg(species) 
-  training <- switch(species, 
-                     "Homo sapiens"=celldex::HumanPrimaryCellAtlasData(), 
-                     "Mus musculus"=celldex::ImmGenData())
-  rows <- intersect(rownames(txis), rownames(training))
-  ref <- trainSingleR(training[rows,], training$label.main)
-
-  # downsample?
   cols <- colnames(txis)
   if (is.null(downsample)) downsample <- (ncol(txis) > 20000)
   if (downsample == TRUE) {
@@ -35,8 +41,16 @@ label_cells <- function(txis, species=c("Homo sapiens", "Mus musculus"), ret=c("
     cols <- sample_by_cluster_and_source(txis, maxcells=maxcells)
   }
 
+  ret <- match.arg(ret)
+  species <- match.arg(species) 
+  training <- switch(species, 
+                     "Homo sapiens"=celldex::HumanPrimaryCellAtlasData(), 
+                     "Mus musculus"=celldex::ImmGenData())
+  rows <- intersect(rownames(txis), rownames(training))
+  ref <- trainSingleR(training, training$label.main)
+
   # now label -- note that this is a ham-fisted default 
-  pred <- SingleR(txis[rows, cols], ref, labels=ref$label, ...)$pruned.labels
+  pred <- SingleR(txis[, cols], ref, labels=ref$label, ...)$pruned.labels
 
   # accommodate downsampling
   colData(txis)[, "celltype.sampled"] <- colnames(txis) %in% cols
