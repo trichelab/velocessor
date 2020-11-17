938ccb2d22fb1b660898bb272ecb4d755ff49d37
Tim Triche, Jr <tim.triche@gmail.com>
2020-11-17 12:07:45 -0500

    compute_velocity fixes

--- R/compute_velocity.R
+++ R/compute_velocity.R
@@ -1,7 +1,7 @@
 #' velocity wrapper, modularizes pipelined loading
 #' 
 #' @param txis    A SingleCellExperiment with assays 'spliced' and 'unspliced'
-#' @param mode    scVelo mode (default "stochastic"; "dynamical" also available)
+#' @param scvmode scVelo mode (default "stochastic"; "dynamical" also available)
 #' @param ...     Additional arguments passed to velociraptor::scvelo 
 #'
 #' @return        A SingleCellExperiment with scVelo output in metadata() 
@@ -12,7 +12,7 @@
 #' @import SingleCellExperiment
 #' 
 #' @export
-compute_velocity <- function(txis, mode="stochastic", ...) { 
+compute_velocity <- function(txis, scvmode="stochastic", ...) { 
 
   message("Removing dead cells and low-variance genes for velocity")
   mt <- names(subset(rowRanges(txis), seqnames %in% c("chrM", "chrMT", "MT")))
@@ -31,19 +31,22 @@ compute_velocity <- function(txis, mode="stochastic", ...) {
 
   message("Adding velocity...") 
   metadata(txis)$scVelo <- 
-    velociraptor::scvelo(txis, subset.row=HVGs, dimred=dimred, mode=mode,
+    velociraptor::scvelo(txis, subset.row=HVGs, dimred=dimred,
                          assay.X="spliced", sf.X=sf$spliced, 
-                         sf.spliced=sf$spliced, sf.unspliced=sf$unspliced, ...) 
+                         sf.spliced=sf$spliced, sf.unspliced=sf$unspliced,
+                         mode=scvmode, ...) 
 
   message("Adding velocity_pseudotime...")
   txis$velocity_pseudotime <- metadata(txis)$scVelo$velocity_pseudotime
  
   # need to 1) fix this and 2) add cellrank support pronto 
-  if (mode == "dynamical") message("Adding latent_time...")
-  if (mode == "dynamical") txis$latent_time <- metadata(txis)$scVelo$latent_time
+  if (scvmode == "dynamical") {
+    message("Adding latent_time...")
+    txis$latent_time <- metadata(txis)$scVelo$latent_time
+  }
     
   message("Embedding velocity onto UMAP coordinates...")
-  if (!"UMAP" %in% reducedDimNames(txis) | ncol(reducedDims(txis)$UMAP) < 3) { 
+  if (!"UMAP" %in% reducedDimNames(txis) | ncol(reducedDim(txis, "UMAP") < 3)) {
     if ("HARMONY" %in% reducedDimNames(txis)) {
       txis <- scater::runUMAP(txis, ncomponents=3, dimred="HARMONY")
     } else { 
@@ -51,10 +54,10 @@ compute_velocity <- function(txis, mode="stochastic", ...) {
     } 
   } 
   embedded <- 
-    velociraptor::embedVelocity(reducedDims(txis)$UMAP, metadata(txis)$scVelo)
+    velociraptor::embedVelocity(reducedDim(txis, "UMAP"), metadata(txis)$scVelo)
   metadata(txis)$embedded <- embedded
 
-  message("Added scVelo (", mode, " mode) output to metadata(txis)$scVelo")
+  message("Added scVelo (", scvmode, " mode) output to metadata(txis)$scVelo")
   return(txis) 
 
 }
