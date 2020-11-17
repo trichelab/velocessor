dfc29d25f3a3ee5bc82a30bff4f07f61f0e39c98
Tim Triche, Jr <tim.triche@gmail.com>
2020-11-17 14:56:40 -0500

    bugfix for plot_velo

--- R/plot_velo.R
+++ R/plot_velo.R
@@ -42,7 +42,7 @@ plot_velo <- function(txis, embed="UMAP", replace=FALSE, colr="velocity_pseudoti
 
   # for colors and point labels 
   dat$SAMPLE <- colData(txis)[rownames(dat), "sample"]
-  dat$COLORING <- colData(txis)[rownames(dat), colr]
+  dat$COLORING <- factor(colData(txis)[rownames(dat), colr])
 
   # for colors and point labels 
   if (colr == "velocity_pseudotime") {
