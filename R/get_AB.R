8bd376357638923daeb21a04a5e615dff1122637
Tim Triche, Jr <tim.triche@gmail.com>
2020-11-17 21:48:26 -0500

    compute_velocity fixes

--- R/get_AB.R
+++ R/get_AB.R
@@ -1,6 +1,7 @@
 #' a shim function
 #'
-#' binarize and TF-IDF, then call boundaries
+#' binarize & TF-IDF SCE, then call boundaries
+#' requires coordinates for the SCE or it will fail 
 #' 
 #' @param   x     something that inherits from RangedSummarizedExperiment
 #' @param   asy   the name of the assay to summarize down ("counts") 
@@ -8,19 +9,23 @@
 #' @param   res   resolution for compartmap bins, in base pairs (1e5)
 #' @param   ...   additional parameters to feed to getATACABsignal 
 #'
-#' @return        AB signal 
+#' @return        AB signal
 #'
-#' @import compartmap
-#' 
+#' @import        compartmap
+#'
+#' @export
 get_AB <- function(x, asy="counts", chr="chr3", res=1e5, ...) { 
 
   gen <- unique(genome(x))[1]
-  if (is.null(gen) | is.na(gen)) gen <- "hg19"
+  if (length(unique(genome(inv3andNBM))) == 0) stop("Your x needs coordinates!")
+  if (is.null(gen) | is.na(gen)) gen <- .getGenomeFromSce(x)
+
   assay(x, paste0("raw", asy)) <- assay(x, asy)
   assay(x, asy) <- t(compartmap::transformTFIDF(assay(x, asy)))
   AB <- getATACABsignal(x, res = res, chr = chr, genome = gen, 
                         num.bootstraps = 100, bootstrap = TRUE, 
                         group = FALSE, boot.parallel = TRUE, boot.cores = 8)
+
   # what's this do? 
   #
   # k562_scatac_group_chr14_100boot.fix <- 
