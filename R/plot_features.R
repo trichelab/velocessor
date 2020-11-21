dc76badb70f558b2324c582a49bee6b4aacbbb68
Tim Triche, Jr <tim.triche@gmail.com>
2020-11-21 12:49:57 -0500

    add default palette for plot_features

--- R/plot_features.R
+++ R/plot_features.R
@@ -5,14 +5,15 @@
 #' @param txis      a SingleCellExperiment
 #' @param features  names of features (rows) to plot
 #' @param joint     for multiple features, plot joint density? (TRUE)
+#' @param pal       what palette to use? ("inferno", because it looks nice)
 #' @param ...       other arguments for Nebulosa::plot_density
 #'
 #' @import SingleCellExperiment
 #' @importFrom Nebulosa plot_density
 #'
 #' @export
-plot_features <- function(txis, features, joint=TRUE, ...) {
+plot_features <- function(txis, features, joint=TRUE, pal="inferno", ...) {
 
-  Nebulosa::plot_density(txis, features, joint=joint, ...)
+  Nebulosa::plot_density(txis, features, joint=joint, pal=pal, ...)
 
 }
