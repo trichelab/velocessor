434121861c020a026d9dfbdce23a93626f9ad991
Tim Triche, Jr <tim.triche@gmail.com>
2020-12-01 17:12:17 -0500

    enhance fit_mixtures and plot_densities

--- R/fit_mixtures.R
+++ R/fit_mixtures.R
@@ -5,34 +5,38 @@
 #' all of the observations into the same group and leave them all eligible. For
 #' prescreening of potential cluster-by-sample combinations, see find_eligible.
 #'
-#' @seealso   find_eligible
+#' @seealso     find_eligible
 #' 
-#' @param x   the thing to fit a 1D mixture to 
-#' @param G   a vector of component numbers (default is 1:2)
-#' @param log take log1p of x? (TRUE) 
+#' @param x     the thing to fit a 1D mixture to 
+#' @param G     vector of component numbers (default is 1:2)
+#' @param log   take log1p of x? (TRUE) 
+#' @param keep  keep the model fits as an attribute? (FALSE) 
 #' 
-#' @return  a list of fits, length 1 if log == FALSE 
+#' @return      list of fits, length 1 if log == FALSE 
 #' 
-#' @import  mclust
+#' @import      mclust
 #' 
 #' @export
-fit_mixtures <- function(x, G=1:2, log=TRUE) { 
+fit_mixtures <- function(x, G=1:2, log=TRUE, keep=FALSE) { 
 
   cellfits <- apply(x, 1, densityMclust, G=G, verbose=FALSE)
   
   if (log) logx <- log1p(x)
   if (log) logcellfits <- apply(logx, 1, densityMclust, G=1:2, verbose=FALSE)
 
+  # possible model names
   modelnames <- c("raw")
   if (log) modelnames <- c("raw", "log")
   names(modelnames) <- modelnames
 
+  # comparisons 
   comps <- data.frame(raw=unname(sapply(cellfits, `[[`, "G")))
   if (log) comps$log <- unname(sapply(logcellfits, `[[`, "G"))
   bics <- data.frame(raw=unname(sapply(cellfits, `[[`, "bic")))
   if (log) bics$log <- unname(sapply(logcellfits, `[[`, "G"))
   rownames(comps) <- rownames(bics) <- names(cellfits)
 
+  # the fits
   fits <- list() 
   for (cl in rownames(x)) {
     if (log) fits[[cl]] <- list(raw=cellfits[[cl]], log=logcellfits[[cl]])
@@ -56,6 +60,8 @@ fit_mixtures <- function(x, G=1:2, log=TRUE) {
       res[cl, ] <- rep(1, ncol(res))
     }
   }
+
+  if (keep) attr(res, "fits") <- fits
   attr(res, "model") <- model
   return(res)
 
--- R/plot_densities.R
+++ R/plot_densities.R
@@ -5,14 +5,22 @@
 #' @seealso fit_mixtures
 #' 
 #' @param fits  the fits from fit_mixtures
+#' @param what  what it is that was fitted
 #' 
 #' @import mclust
 #' 
 #' @export
-plot_densities <- function(fits) { 
+plot_densities <- function(fits, what="cells") { 
 
-  par(mfrow=c(1,2))
-  plotDensityMclust1(fits$raw, data=fits$raw$data, xlab="cells")
-  plotDensityMclust1(fits$log, data=fits$log$data, xlab="log1p(cells)")
+  par(mfrow=c(1, length(fits)))
+
+  # the default
+  plotDensityMclust1(fits$raw, data=fits$raw$data, xlab=what)
+
+  # generalize?
+  if ("log" %in% names(fits)) {
+    plotDensityMclust1(fits$log, data=fits$log$data, 
+                       xlab=paste0("log1p(", what, ")"))
+  }
 
 } 
