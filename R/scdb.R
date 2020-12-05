4143a1a634fe0384f24dac411f7299ba3fd5e4c0
Tim Triche, Jr <tim.triche@gmail.com>
2020-12-05 14:38:08 -0500

    add scdb()

---
+++ R/scdb.R
@@ -0,0 +1,69 @@
+#' automates fetching of Valentine Svenssen's single-cell database
+#' 
+#' scdb() tidies up the database a bit, with Date, URL, Cells, and Velocity
+#'
+#' @details
+#' The database itself lives at http://www.nxn.se/single-cell-studies/data.tsv
+#' 
+#' @references 
+#' Svensson V, Beltrame EdV, Pachter L (2020), A curated database reveals 
+#' trends in single-cell transcriptomics, Database, 28 November 2020
+#' https://doi.org/10.1093/database/baaa073
+#'
+#' @param URL   the URL if it changes 
+#' @param ...   anything else to tell utils::read.delim
+#' 
+#' @return      a data.frame, with the Date column as an actual Date
+#'
+#' @examples 
+#' 
+#'   db <- scdb()
+#'
+#'   library(useful)
+#'   library(tidyverse)
+#'   subset(db, Velocity == TRUE) %>% corner
+#'
+#'   columns <- c("Title", "Data.location", "URL")
+#'   subset(db, Tissue == "Blood" & Cells > 50000)[, columns]
+#'   subset(db, grepl("CITE", Technique))[, columns]
+#'   subset(db, grepl("Amit", Authors))[, columns]
+#' 
+#'   a_year_ago <- (Sys.Date() - 365)
+#'   subset(db, Date > a_year_ago) %>% corner
+#'
+#'   five_years_ago <- (Sys.Date() - (5*365.25))
+#'   p1 <- ggplot(subset(db, Date > five_years_ago), 
+#'                aes(x = Date, fill = Velocity)) + 
+#'     geom_histogram() + 
+#'     scale_x_date() + 
+#'     xlab("year") + 
+#'     scale_y_log10() + 
+#'     ylab("papers") + 
+#'     NULL 
+#' 
+#'   p1 + theme_minimal() + ggtitle("Velocity in papers")
+#' 
+#'   p2 <- ggplot(subset(db, 
+#'                       Organism %in% c("Human", "Mouse", "Zebrafish") & 
+#'                       Date > five_years_ago), 
+#'                aes(x=Date, y=Cells, color=Organism, group=Organism)) + 
+#'     geom_point() + 
+#'     geom_smooth() + 
+#'     scale_y_log10() + 
+#'     NULL
+#' 
+#'   p2 + facet_grid(vars(Organism)) + theme_minimal() 
+#' 
+#' @import utils
+#' 
+#' @export 
+scdb <- function(URL="http://www.nxn.se/single-cell-studies/data.tsv", ...){
+
+  db <- utils::read.delim(URL, ...)
+  db$Date <- as.Date(as.character(db$Date), "%Y%m%d")
+  db$URL <- paste("http://dx.doi.org", db$DOI, sep="/")
+  db$Cells <- as.integer(gsub(",", "", db$Reported.cells.total)) 
+  db$Velocity <- (scdb$RNA.Velocity == "Yes")
+  return(db)
+
+}
