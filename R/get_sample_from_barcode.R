a3ed4713f5f564c521f2952b2961bb54e6d15dc6
Tim Triche, Jr <tim.triche@gmail.com>
2020-12-09 16:00:09 -0500

    make get_sample_from_barcode more useful

--- R/get_sample_from_barcode.R
+++ R/get_sample_from_barcode.R
@@ -1,24 +1,33 @@
-#' extract the sample/batch name for each cell from a SAMPLENAME_BARCODE scheme
+#' Extract the sample/batch name for each cell from a SAMPLENAME_BARCODE scheme
 #' 
-#' This function simply assumes that process_velo_txis has renamed the cell 
-#' barcodes in a SingleCellExperiment to indicate where they came from, and 
-#' pops the last element of the split vector off the end.  This is handy for 
-#' automatically running Harmony as a first pass to get rid of batch effects, 
-#' though the right way to handle that task while respecting spliced-unspliced
-#' relationships is currently an open research problem. 
+#' This function assumes that process_velo_txis or similar has renamed the cell 
+#' barcodes (colnames) in a SingleCellExperiment to indicate whence they came, 
+#' and pops the last element of the split vector off the end.  This is handy 
+#' for running Harmony as a first pass to get rid of batch effects, although 
+#' the right way to do this while respecting spliced-unspliced relationships 
+#' is currently an open research problem. 
 #' 
-#' @param txis  a SingleCellExperiment with colnames(txis) like FOO_BAR_AATGCT
+#' @param txis  SingleCellExperiment or vector with entries like FOO_BAR_AATGCT
 #' @param sep   the separator character used to paste the column names ("_")
 #' 
 #' @return      a character vector of sample names, e.g. FOO_BAR
 #' 
-#' @import SingleCellExperiment
+#' @import      SingleCellExperiment
 #' 
 #' @export
 get_sample_from_barcode <- function(txis, sep="_") {
-  vapply(strsplit(colnames(txis), sep), .popOff, character(1))
+  if (is(txis, "SingleCellExperiment")) .getSampleNames(colnames(txis), sep=sep)
+  else .getSampleNames(txis, sep=sep)
 }
 
 
 # helper fn
-.popOff <- function(x, sep="_") paste(x[-length(x)], collapse=sep)
+.getSampleNames <- function(x, sep="_") { 
+  vapply(strsplit(x, sep), .popOff, sep=sep, character(1))
+}
+
+
+# helper fn
+.popOff <- function(x, sep="_") {
+  paste(x[-length(x)], collapse=sep)
+}
